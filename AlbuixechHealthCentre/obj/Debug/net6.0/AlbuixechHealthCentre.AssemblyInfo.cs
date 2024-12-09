63cecda6fcaf403bdc66cf917f7e776135c38533
Juan Bta. Peris Fontestad <jbperfon@epsg.upv.es>
2024-12-09 23:58:16 +0100

    Añadida BD + conexión de prueba correcta.
    se añade la base de datos con una prueba para comprobar que conecta.

--- AlbuixechHealthCentre/obj/Debug/net6.0/AlbuixechHealthCentre.AssemblyInfo.cs
+++ AlbuixechHealthCentre/obj/Debug/net6.0/AlbuixechHealthCentre.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlbuixechHealthCentre")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ba0c558813e4cfe89521ed8b808069a3f32223")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlbuixechHealthCentre")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlbuixechHealthCentre")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
